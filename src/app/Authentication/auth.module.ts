429e74c7d5f06b0f97996df40fb92c60d996f52e
alireza <alirezaebrahimkhani1973@gmail.com>
2019-09-14 12:54:34 +0430

    Authentication
    signin/up compeleted ...

--- src/app/Authentication/auth.module.ts
+++ src/app/Authentication/auth.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
 import { SignInComponent } from './components/sign-in/sign-in.component';
-import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-    declarations:[SignInComponent,SignUpComponent],
+    declarations:[SignInComponent],
     imports:[
         CommonModule,
         SharedModule
     ],
     exports:[
-        SignInComponent,
-        SignUpComponent
+        SignInComponent
     ],
     providers:[]
 })
